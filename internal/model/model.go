43dd2214855746678b60a997308dd1463ca80dc5
kanyuanzhi <kan@mail.ustc.edu.cn>
2021-12-16 15:26:00 +0800

    update

--- internal/model/model.go
+++ internal/model/model.go
@@ -3,5 +3,5 @@ package model
 type DefaultFields struct {
 	ID        uint  `json:"id" gorm:"primary_key"`
 	CreatedAt int64 `json:"created_at" gorm:"autoCreateTime:milli"` // gorm自动使用当前时间戳的秒数填充
-	UpdatedAt int   `json:"updated_at" gorm:"autoUpdateTime:milli"`
+	UpdatedAt int64 `json:"updated_at" gorm:"autoUpdateTime:milli"`
 }
